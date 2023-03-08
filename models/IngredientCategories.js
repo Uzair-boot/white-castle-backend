cc5abed3772fe3469ac13f75e649055d33e17b7a
hamza <hamzabyte@gmail.com>
2023-03-08 16:36:38 +0500

    Ingredients API CRUD

---
+++ models/IngredientCategories.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const IngredientSchema = new mongoose.Schema({
+    name : {
+        type: String,
+        required: true,
+        max: 50,
+        unique: true,
+    },
+
+    desc:{
+        type: String,
+        max: 50
+    },
+},
+
+{timestamps: true}
+)
+
+module.exports = mongoose.model("IngredientCategory", IngredientSchema);
